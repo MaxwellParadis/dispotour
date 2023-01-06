85115f243e218d4764a43e943e6962de2ba720bb
maxwell <maxwell.m.paradis@gmail.com>
2023-01-06 22:55:16 +0000

    initial commit

---
+++ disposition/src/App.svelte
@@ -0,0 +1,42 @@
+<script>
+  import { onMount } from "svelte";
+  import { createScene } from "./scene";
+
+  const items = [{ name: "Test", img: "./img/pdemo2.jpg" }, { name: "Test", img: "./img/pdemo2.jpg" }, { name: "Test", img: "./img/pdemo2.jpg" }, { name: "Test", img: "./img/pdemo2.jpg" }];
+
+  function handleTour(tour) {
+    t.currentTarget.nextElementSibling.classList.toggle("close");
+  }
+
+  function handleTab(mode) {
+    t.currentTarget.nextElementSibling.classList.toggle("close");
+  }
+
+  function handleUpdate() {
+    t.currentTarget.nextElementSibling.classList.toggle("close");
+  }
+  
+  let lon;
+  onMount(() => {
+    createScene(lon);
+  });
+</script>
+
+<body>
+  <div class="head" />
+  <canvas bind:this={lon} />
+  <div class="base">
+    <div class="base-butt">
+      <button class="base-tab">Tours</button>
+      <button class="base-tab">Edit</button>
+    </div>
+    <div class="base-bar">
+      {#each items as item, i}
+        <button class="tiles" on:click={handleTour}>
+          {item.name}
+          <img alt={item.name} class=timg src={item.img}/>
+        </button>
+      {/each}
+    </div>
+  </div>
+</body>
---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var server = express();
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({extended:true}));
+server.use(express.json());
+
+server.use(express.static('disposition/public'));
+
+require('dotenv').config();
+const PORT = process.env.PORT;
+
+server.get("/", (req, res) => {
+	res.sendFile('index.html');
+});
+
+server.listen(PORT, () => {
+	console.log('Server Running on '+PORT);
+});
